b94a66827a1a502c1c9535f2da5afce8d9ece02d
nppe6 <xiao2313667401@foxmail.com>
2025-01-27 20:08:11 +0800

    工具搭建完成

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  framework: ['express', 'vue'],
+  frameworkUrl: {
+    express: 'git@gitee.com:nppe6/express-template.git',
+    vue: 'git@gitee.com:nppe6/xy-vue-admin.git'
+  }
+}
