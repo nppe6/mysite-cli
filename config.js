036896bbdd197de6d7b4f1e0ffb690e5563e4c3e
nppe6 <xiao2313667401@foxmail.com>
2025-04-10 21:58:57 +0800

    feat：添加uniapp模板

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  framework: ['tinyHttp', 'Nuxt', 'vue'],
+  framework: ['tinyHttp', 'Nuxt', 'vue', 'uniapp'],
   frameworkUrl: {
     tinyHttp: 'git@gitee.com:nppe6/tiny-template.git',
     vue: 'git@gitee.com:nppe6/xy-vue-admin.git',
-    Nuxt: 'git@gitee.com:nppe6/nuxt-template.git'
+    Nuxt: 'git@gitee.com:nppe6/nuxt-template.git',
+    uniapp: 'git@gitee.com:nppe6/uni-template.git'
   }
 }
