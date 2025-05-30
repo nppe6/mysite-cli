8bea3a6eb529577206df09f8ed6afe56aa82c1f8
nppe6 <xiao2313667401@foxmail.com>
2025-05-30 21:40:46 +0800

    feat：新增vue后台ts版本库地址

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ module.exports = {
   framework: ['koa', 'Nuxt', 'vue', 'uniapp'],
   frameworkUrl: {
     koa: 'git@gitee.com:nppe6/koa-template.git',
-    vue: 'git@gitee.com:nppe6/xy-vue-admin.git',
+    vue3: 'git@gitee.com:nppe6/xy-vue-admin.git',
+    vue_cms: 'git@gitee.com:nppe6/vue3-ts-template.git',
+    vue_front: 'git@gitee.com:nppe6/xy-vue-admin.git',
     Nuxt: 'git@gitee.com:nppe6/nuxt-template.git',
     uniapp: 'git@gitee.com:nppe6/uni-template.git'
   }
