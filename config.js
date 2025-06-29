316594111db600ba182fe6bbf85fa2b6f8137a05
nppe6 <xiao2313667401@foxmail.com>
2025-06-29 11:56:58 +0800

    feat: 更新仓库

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  framework: ['koa', 'Nuxt', 'vue', 'vue_cms', 'uniapp'],
+  framework: ['koa', 'koa_mysql', 'uniapp', 'vue_front', 'vue_cms'],
   frameworkUrl: {
     koa: 'git@gitee.com:nppe6/koa-template.git',
-    vue3: 'git@gitee.com:nppe6/xy-vue-admin.git',
-    vue_cms: 'git@gitee.com:nppe6/naive-admin-pro.git',
+    koa_mysql: 'git@gitee.com:nppe6/koa-mysql-template.git',
+    uniapp: 'git@gitee.com:nppe6/uni-vue3-template.git',
     vue_front: 'git@gitee.com:nppe6/xy-vue-admin.git',
-    Nuxt: 'git@gitee.com:nppe6/nuxt-template.git',
-    uniapp: 'git@gitee.com:nppe6/uni-vue3-template.git'
+    vue_cms: 'git@gitee.com:nppe6/naive-admin-pro.git'
+    // vue3: 'git@gitee.com:nppe6/xy-vue-admin.git',
+    // Nuxt: 'git@gitee.com:nppe6/nuxt-template.git',
   }
 }
