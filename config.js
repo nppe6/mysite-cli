9edc46f1cd423359d4b29b961388f6f2c18247a4
nppe6 <xiao2313667401@foxmail.com>
2025-08-17 16:11:18 +0800

    feat: 更新uniapp仓库地址

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   frameworkUrl: {
     koa: 'git@gitee.com:nppe6/koa-template.git',
     koa_mysql: 'git@gitee.com:nppe6/koa-mysql-template.git',
-    uniapp: 'git@gitee.com:nppe6/uni-vue3-template.git',
+    uniapp: 'git@gitee.com:nppe6/uni-template-ts.git',
     vue_front: 'git@gitee.com:nppe6/naive-pro.git',
     vue_cms: 'git@gitee.com:nppe6/naive-admin-pro.git'
     // vue3: 'git@gitee.com:nppe6/xy-vue-admin.git',
