9b904762c5592c549256e0e7cc9ad8d2d65b26dc
nppe6 <xiao2313667401@foxmail.com>
2025-10-23 23:09:56 +0800

    feat: 添加electron框架到配置文件中
    添加electron框架及其git仓库地址到配置中，以支持electron项目的模板生成

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  framework: ['koa', 'koa_mysql', 'uniapp', 'vue_front', 'vue_cms'],
+  framework: ['koa', 'koa_mysql', 'uniapp', 'vue_front', 'vue_cms', 'electron'],
   frameworkUrl: {
     koa: 'git@gitee.com:nppe6/koa-template.git',
     koa_mysql: 'git@gitee.com:nppe6/koa-mysql-template.git',
     uniapp: 'git@gitee.com:nppe6/uni-template-ts.git',
     vue_front: 'git@gitee.com:nppe6/naive-pro.git',
-    vue_cms: 'git@gitee.com:nppe6/naive-admin-pro.git'
+    vue_cms: 'git@gitee.com:nppe6/naive-admin-pro.git',
+    electron: 'git@gitee.com:nppe6/basic-electron.git'
     // vue3: 'git@gitee.com:nppe6/xy-vue-admin.git',
     // Nuxt: 'git@gitee.com:nppe6/nuxt-template.git',
   }
