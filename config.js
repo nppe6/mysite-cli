d3804355add296f3c43a7ec9c6917036f2e1e351
nppe6 <xiao2313667401@foxmail.com>
2025-11-09 13:08:07 +0800

    refactor(config): 更新框架配置并移除未使用的模板

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  framework: ['koa', 'koa_mysql', 'uniapp', 'vue_front', 'vue_cms', 'electron'],
+  framework: ['nestjs', 'koa', 'koa_mysql', 'uniapp'],
   frameworkUrl: {
-    koa: 'git@gitee.com:nppe6/koa-template.git',
-    koa_mysql: 'git@gitee.com:nppe6/koa-mysql-template.git',
-    uniapp: 'git@gitee.com:nppe6/uni-template-ts.git',
-    vue_front: 'git@gitee.com:nppe6/naive-pro.git',
-    vue_cms: 'git@gitee.com:nppe6/naive-admin-pro.git',
-    electron: 'git@gitee.com:nppe6/basic-electron.git'
+    nestjs: 'https://gitee.com/nppe6/nest-cli.git',
+    koa: 'https://gitee.com/nppe6/koa-template.git',
+    koa_mysql: 'https://gitee.com/nppe6/koa-mysql-template.git',
+    uniapp: 'https://gitee.com/nppe6/uni-template-ts.git'
+    // vue_front: 'git@gitee.com:nppe6/naive-pro.git',
+    // vue_cms: 'git@gitee.com:nppe6/naive-admin-pro.git',
+    // electron: 'git@gitee.com:nppe6/basic-electron.git'
     // vue3: 'git@gitee.com:nppe6/xy-vue-admin.git',
     // Nuxt: 'git@gitee.com:nppe6/nuxt-template.git',
   }
